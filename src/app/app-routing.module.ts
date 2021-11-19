980126151e61077dd8fbcb3b2bdc8b160bb0fe6b
megha angadi <meghamangadi@gmail.com>
2021-11-19 09:56:05 +0530

    Added : Employee Payroll Frontend

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+
+const routes: Routes = [
+  {
+    path: '' , component: DashBoardComponent},
+   { path: 'addemployee', component: AddEmployeeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashBoardComponent,
+    AddEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
